5fa58801b7bfd0e3d5f0d09b15d96d7637569d40
Slavik <vjacheslav.horbach@gmail.com>
2017-02-12 19:51:22 +0000

    Initial Commit

---
+++ FlickrSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by Vyacheslav Horbach on 12/02/17.
+//  Copyright © 2017 Vjaceslav Horbac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
