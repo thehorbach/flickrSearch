9133a04d64f32e93b10394d70506788406f587a9
Slavik <vjacheslav.horbach@gmail.com>
2017-02-13 01:10:46 +0000

    implement UICollectionViewDatasource + UICollectionViewDelegateFlowLayout

--- FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -12,20 +12,65 @@ final class FlickrPhotosViewController: UICollectionViewController {
     
     // MARK: - Properties
     fileprivate let reuseIdentifier = "FlickrCell"
-    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     fileprivate var searches = [FlickrSearchResults]()
     fileprivate let flickr = Flickr()
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    fileprivate let itemsPerRow: CGFloat = 3
+}
+
+
+// MARK: - UICollectionViewDataSource
+extension FlickrPhotosViewController {
     
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension FlickrPhotosViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
 }
 
 // MARK: - Private
 private extension FlickrPhotosViewController {
+    
     func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
         return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
     }
+    
 }
 
+// MARK: - UITexFieldDelegate
 extension FlickrPhotosViewController: UITextFieldDelegate {
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         
         let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
