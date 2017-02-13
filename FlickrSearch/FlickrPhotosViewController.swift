03dd728bc77178574337323f1b2dd216bac1617d
Slavik <vjacheslav.horbach@gmail.com>
2017-02-13 00:37:29 +0000

    initial commit

---
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,59 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by Vyacheslav Horbach on 12/02/17.
+//  Copyright © 2017 Vjaceslav Horbac. All rights reserved.
+//
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    fileprivate var searches = [FlickrSearchResults]()
+    fileprivate let flickr = Flickr()
+    
+}
+
+// MARK: - Private
+private extension FlickrPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
+}
+
+extension FlickrPhotosViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) { (results, error) in
+            
+            activityIndicator.removeFromSuperview()
+            
+            
+            if let error = error {
+                print("Error searching : \(error)")
+                return
+            }
+            
+            if let results = results {
+                
+                print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+                self.searches.insert(results, at: 0)
+
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
